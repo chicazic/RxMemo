0a8924878658895a7a519e8cdce5b06436d424e2
woo_gi <chicazic@gmail.com>
2020-10-04 21:20:51 +0900

    RxSwift, MVVM으로 메모 앱 만들기 #3

---
+++ RxMemo/RxMemo/Model/Memo.swift
@@ -0,0 +1,25 @@
+//
+//  Memo.swift
+//  RxMemo
+//
+//  Created by 송형욱 on 2020/10/04.
+//
+
+import Foundation
+
+struct Memo: Equatable {
+    var content: String
+    var insertDate: Date
+    var identity: String
+    
+    init(content: String, insertDate: Date = Date()) {
+        self.content = content
+        self.insertDate = insertDate
+        self.identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    init(original: Memo, updatedContent: String) {
+        self = original
+        self.content = updatedContent
+    }
+}
---
+++ RxMemo/RxMemo/Service/MemoStorageType.swift
@@ -0,0 +1,23 @@
+//
+//  MemoStorageType.swift
+//  RxMemo
+//
+//  Created by 송형욱 on 2020/10/04.
+//
+
+import Foundation
+import RxSwift
+
+protocol MemoStorageType {
+    @discardableResult
+    func createMemo(content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func MemoList() -> Observable<[Memo]>
+    
+    @discardableResult
+    func update(memo: Memo, content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func delete(memo: Memo) -> Observable<Memo>
+}
